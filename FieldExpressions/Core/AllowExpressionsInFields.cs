635b13e0b27888326848ff2e755ef23fa49463ed
Toxic_Cookie <66691163+Toxic-Cookie@users.noreply.github.com>
2022-04-06 10:42:15 -0700

    CRITICAL FIX FOR RCE! UPDATE IMMEDIATELY!

--- FieldExpressions/Core/AllowExpressionsInFields.cs
+++ FieldExpressions/Core/AllowExpressionsInFields.cs
@@ -44,7 +44,7 @@ namespace FieldExpressions.Core
                         {
                             if (structFieldAccessor.TargetType != typeof(string))
                             {
-                                if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(evaluator.ScriptEvaluate(____textEditor.Target.Text.Target.Text + ";").ToString(), out var _parsed))
+                                if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(evaluator.Evaluate(____textEditor.Target.Text.Target.Text + ";").ToString(), out var _parsed))
                                 {
                                     __instance.SetMemberValue(_parsed);
                                 }
