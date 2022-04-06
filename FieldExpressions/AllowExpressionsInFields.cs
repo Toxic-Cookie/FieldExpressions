d6c18786cf6d30568b8805d71d66fb882584cf7c
ljoonal <ljoonal@ljoonal.xyz>
2022-04-06 23:34:27 +0300

    Use Ncalc instead

--- FieldExpressions/AllowExpressionsInFields.cs
+++ FieldExpressions/AllowExpressionsInFields.cs
@@ -1,78 +1,73 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using CodingSeb.ExpressionEvaluator;
-using HarmonyLib;
-using NeosModLoader;
 using BaseX;
 using FrooxEngine;
+using HarmonyLib;
+using NCalc;
+using NeosModLoader;
 
-namespace FieldExpressions.Core
+namespace FieldExpressions
 {
-    class FieldExpressions : NeosMod
-    {
-        public override string Name => "FieldExpressions";
-        public override string Author => "Toxic_Cookie";
-        public override string Version => "1.0.2";
-        public override string Link => "https://github.com/Toxic-Cookie/FieldExpressions";
+	class FieldExpressions : NeosMod
+	{
+		public override string Name => "FieldExpressions";
+		public override string Author => "Toxic_Cookie";
+		public override string Version => "1.0.2";
+		public override string Link => "https://github.com/Toxic-Cookie/FieldExpressions";
 
-        public override void OnEngineInit()
-        {
-            Harmony harmony = new Harmony("net.Toxic_Cookie.FieldExpressions");
-            harmony.PatchAll();
+		public override void OnEngineInit()
+		{
+			Harmony harmony = new Harmony("net.Toxic_Cookie.FieldExpressions");
+			harmony.PatchAll();
 
-            evaluator.OptionCaseSensitiveEvaluationActive = false;
-        }
+			Expression.CacheEnabled = false;
+		}
 
-        public static ExpressionEvaluator evaluator = new ExpressionEvaluator();
+		[HarmonyPatch(typeof(PrimitiveMemberEditor), "ParseAndAssign")]
+		class AllowExpressionsInFields
+		{
+			static bool Prefix(PrimitiveMemberEditor __instance, ref SyncRef<TextEditor> ____textEditor, ref FieldDrive<string> ____textDrive)
+			{
+				try
+				{
+					StructFieldAccessor structFieldAccessor = __instance.Accessor;
 
-        [HarmonyPatch(typeof(PrimitiveMemberEditor), "ParseAndAssign")]
-        class AllowExpressionsInFields
-        {
-            static bool Prefix(PrimitiveMemberEditor __instance, ref SyncRef<TextEditor> ____textEditor, ref FieldDrive<string> ____textDrive)
-            {
-                try
-                {
-                    StructFieldAccessor structFieldAccessor = __instance.Accessor;
+					if (structFieldAccessor != null)
+					{
+						try
+						{
+							if (structFieldAccessor.TargetType != typeof(string))
+							{
+								Expression e = new Expression(____textEditor.Target.Text.Target.Text.Trim('\r', '\n'), EvaluateOptions.IgnoreCase);
+								if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(e.Evaluate().ToString(), out var _parsed))
+								{
+									__instance.SetMemberValue(_parsed);
+								}
+							}
+							else
+							{
+								if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(____textEditor.Target.Text.Target.Text, out var parsed))
+								{
+									__instance.SetMemberValue(parsed);
+								}
+							}
+						}
+						catch (Exception ex)
+						{
+							Debug(ex);
+							if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(____textEditor.Target.Text.Target.Text, out var parsed))
+							{
+								__instance.SetMemberValue(parsed);
+							}
+						}
+					}
+				}
+				catch (Exception arg)
+				{
+					UniLog.Error($"Exception assigning value to {____textDrive.Target}:\n{arg}");
+				}
 
-                    if (structFieldAccessor != null)
-                    {
-                        try
-                        {
-                            if (structFieldAccessor.TargetType != typeof(string))
-                            {
-                                if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(evaluator.Evaluate(____textEditor.Target.Text.Target.Text).ToString(), out var _parsed))
-                                {
-                                    __instance.SetMemberValue(_parsed);
-                                }
-                            }
-                            else
-                            {
-                                if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(____textEditor.Target.Text.Target.Text, out var parsed))
-                                {
-                                    __instance.SetMemberValue(parsed);
-                                }
-                            }
-                        }
-                        catch
-                        {
-                            if (PrimitiveTryParsers.GetParser(structFieldAccessor.TargetType)(____textEditor.Target.Text.Target.Text, out var parsed))
-                            {
-                                __instance.SetMemberValue(parsed);
-                            }
-                        }
-                    }
-                }
-                catch (Exception arg)
-                {
-                    UniLog.Error($"Exception assigning value to {____textDrive.Target}:\n{arg}");
-                }
-
-                return false;
-            }
-        }
-    }
+				return false;
+			}
+		}
+	}
 }
